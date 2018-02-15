26822186b509a67a3b4e381dceff385eae7b7870
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-15 22:51:50 +0100

    Modify scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 function getTriangleArea(a, h) {
-  if ( a <= 0  ||  h <= 0 ) {
-  alert('Nieprawidłowe dane!')
-  } else {
-  return a*h/2;
-  }
+    if ( a <= 0  ||  h <= 0 ) {
+        alert('Nieprawidłowe dane!');
+}   else {
+        return a*h/2;
+    }
 }
-console.log(getTriangleArea(10, 6));
-
-var triangle1Area = getTriangleArea(10, 15);
-conole.log(triangle1Area);
-var triangle2Area = getTriangleArea(10, 10);
-conole.log(triangle2Area);
-var triangle3Area = getTriangleArea(9, 8);
-conole.log(triangle3Area);
+  console.log(getTriangleArea(10, 6));
+  
+  var triangle1Area = getTriangleArea(10, 15);
+  console.log(triangle1Area);
+  var triangle2Area = getTriangleArea(10, 10);
+  console.log(triangle2Area);
+  var triangle3Area = getTriangleArea(9, 8);
+  console.log(triangle3Area);
