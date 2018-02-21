d4b7ba5b60f19dc374946d4468d6250a4f2da778
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-21 19:39:59 +0100

    Indentation once again

--- scripts.js
+++ scripts.js
@@ -1,15 +1,15 @@
 function getTriangleArea(a, h) {
-    if ( a <= 0  ||  h <= 0 ) {
+    if (a <= 0 || h <= 0) {
         alert('Nieprawidłowe dane!');
     } else {
-        return a*h/2;
+        return a * h / 2;
     }
 }
-  console.log(getTriangleArea(10, 6));
-  
-  var triangle1Area = getTriangleArea(10, 15);
-  console.log(triangle1Area);
-  var triangle2Area = getTriangleArea(10, 10);
-  console.log(triangle2Area);
-  var triangle3Area = getTriangleArea(9, 8);
-  console.log(triangle3Area);
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(10, 15);
+console.log(triangle1Area);
+var triangle2Area = getTriangleArea(10, 10);
+console.log(triangle2Area);
+var triangle3Area = getTriangleArea(9, 8);
+console.log(triangle3Area);
